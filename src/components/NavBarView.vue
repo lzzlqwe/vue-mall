465ded4c8cd47c8d760836c02083060e8e8d2816
lzz <972321170@qq.com>
2025-01-26 18:26:19 +0800

    添加logo

--- src/components/NavBarView.vue
+++ src/components/NavBarView.vue
@@ -1,7 +1,7 @@
 <template>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">LOGO</a>
+    <img class="navbar-brand" href="#" src="@/static/img/tiktok.png" width="100em"/>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
